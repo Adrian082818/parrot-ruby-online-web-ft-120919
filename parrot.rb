763a063ca36377e2416306a50fa246613373e039
Adrian Mohabir <mohabiradrian@gmail.com>
2021-04-19 22:20:10 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+<<<<<<< HEAD
 # def parrot(string = "Squawk!")
 #   puts "#{string}"
 #   return string
@@ -11,3 +12,9 @@ def parrot(string = "Squawk!")
 end 
 
 # retesting if lab working 
+=======
+def parrot(string = "Squawk!")
+  puts "#{string}"
+  return string
+end 
+>>>>>>> 385bc1dc6a85c8f874656290423e54d2382db386
